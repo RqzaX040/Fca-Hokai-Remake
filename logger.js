f4a395c6fa1ac26fafc3b97c859da8d329a3bc7f
Văn Thuận <107206503+RqzaX040@users.noreply.github.com>
2022-08-11 15:22:14 +0700

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,65 @@
+/* eslint-disable linebreak-style */
+
+const chalk = require('chalk');
+var isHexcolor = require('is-hexcolor');
+var getText = function(/** @type {string[]} */ ...Data) {
+	var Main = (Data.splice(0,1)).toString();
+		for (let i = 0; i < Data.length; i++) Main = Main.replace(RegExp(`%${i + 1}`, 'g'), Data[i]);
+	return Main;
+};
+/**
+ * @param {any} obj
+ */
+function getType(obj) {
+    return Object.prototype.toString.call(obj).slice(8, -1);
+}
+
+module.exports = {
+	Normal: function(/** @type {string} */ Str, /** @type {() => any} */ Data ,/** @type {() => void} */ Callback) {
+		if (isHexcolor(globalThis.Fca.Require.FastConfig.MainColor) != true) {
+			this.Warning(getText(globalThis.Fca.Require.Language.Index.InvaildMainColor,globalThis.Fca.Require.FastConfig.MainColor),process.exit(0));
+		}
+		else console.log(chalk.hex(globalThis.Fca.Require.FastConfig.MainColor).bold(`${globalThis.Fca.Require.FastConfig.MainName || '[ FCA-HOK ]'} > `) + Str);
+		if (getType(Data) == 'Function' || getType(Data) == 'AsyncFunction') {
+			return Data()
+		}
+		if (Data) {
+			return Data;
+		}
+		if (getType(Callback) == 'Function' || getType(Callback) == 'AsyncFunction') {
+			Callback();
+		}
+		else return Callback;
+	},
+	Warning: function(/** @type {unknown} */ str, /** @type {() => void} */ callback) {
+		console.log(chalk.magenta.bold('[ FCA-WARNING ] > ') + chalk.yellow(str));
+		if (getType(callback) == 'Function' || getType(callback) == 'AsyncFunction') {
+			callback();
+		}
+		else return callback;
+	},
+	Error: function(/** @type {unknown} */ str, /** @type {() => void} */ callback) {
+		if (!str) {
+			console.log(chalk.magenta.bold('[ FCA-ERROR ] > ') + chalk.red("Already Faulty, Please Contact: Facebook.com/RqzaX.VqnThuqn.040"));
+		}
+		console.log(chalk.magenta.bold('[ FCA-ERROR ] > ') + chalk.red(str));
+		if (getType(callback) == 'Function' || getType(callback) == 'AsyncFunction') {
+			callback();
+		}
+		else return callback;
+	},
+	Success: function(/** @type {unknown} */ str, /** @type {() => void} */ callback) {
+		console.log(chalk.hex('#9900FF').bold(`${globalThis.Fca.Require.FastConfig.MainName || '[ FCA-HOK ]'} > `) + chalk.green(str));
+		if (getType(callback) == 'Function' || getType(callback) == 'AsyncFunction') {
+			callback();
+		}
+		else return callback;
+	},
+	Info: function(/** @type {unknown} */ str, /** @type {() => void} */ callback) {
+		console.log(chalk.hex('#9900FF').bold(`${globalThis.Fca.Require.FastConfig.MainName || '[ FCA-HOK ]'} > `) + chalk.blue(str));
+		if (getType(callback) == 'Function' || getType(callback) == 'AsyncFunction') {
+			callback();
+		}
+		else return callback;
+	}
+}
